d9bb4b7839f505c93bb16167e9cae4134ce5a761
Maobugichi <immansamuel846@gmail.com>
2025-01-18 01:01:49 -0800

    final commit

--- src/components/SkillBox.jsx
+++ src/components/SkillBox.jsx
@@ -1,6 +1,6 @@
 const SkillBox = ({children}) => {
     return(
-        <div className="w-[125px] lg:w-[130px] flex flex-col items-center justify-center border h-[50px] hover:bg-[#61b2e4] transition-colors duration-700 ease-out">{children}</div>
+        <div className="w-[115px] lg:w-[130px] flex flex-col items-center justify-center border h-[50px] hover:bg-[#61b2e4] transition-colors duration-700 ease-out">{children}</div>
     )
 }
 
